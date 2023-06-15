70877d4bf0e8b4c0db0ae2903a716b2330d4a8b3
Adi <Ahmadifatek@gmail.com>
2023-06-15 14:10:46 +0800

    Add files via upload
    Upload tugas prkatikum delphi 1A

---
+++ Praktikum.dpr
@@ -0,0 +1,13 @@
+program Praktikum;
+
+uses
+  Forms,
+  Ulatihan1 in 'Ulatihan1.pas' {Form1};
+
+{$R *.res}
+
+begin
+  Application.Initialize;
+  Application.CreateForm(TForm1, Form1);
+  Application.Run;
+end.
